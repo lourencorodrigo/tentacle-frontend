c555936bf7a7cde73a2b16f11c75f167ba95d93d
Rodrigo Lourenço <rsilvape@gmail.com>
2019-01-22 23:36:21 -0300

    Shadow adjustment

--- src/containers/ServiceWorker/styles.js
+++ src/containers/ServiceWorker/styles.js
@@ -35,7 +35,7 @@ export const Wrapper = styled.div`
   position: absolute;
   left: 0;
   right: 0;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+  box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 4px;
   display: flex;
   justify-content: center;
   padding: 6px;
